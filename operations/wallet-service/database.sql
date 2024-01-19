f00a31916c4568272d39fa2c56fedd378d5a09ca
Shayan Malinda <shayanmalinda@gmail.com>
2024-01-19 14:51:57 +0530

    Adding license headers

--- operations/wallet-service/database.sql
+++ operations/wallet-service/database.sql
@@ -1,3 +1,8 @@
+-- Copyright (c) 2024, WSO2 LLC. (http://www.wso2.com). All Rights Reserved.
+-- This software is the property of WSO2 LLC. and its suppliers, if any.
+-- Dissemination of any information or reproduction of any material contained
+-- herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+-- You may not alter or remove any copyright or other notice from copies of this content.
 CREATE DATABASE wso2_wallet;
 
 DROP TABLE IF EXISTS user_wallet;
